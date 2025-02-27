a199757b38b19901ab6fce897a9c79c9481c6f58
Oswaldo Rios <chomper.or@gmail.com>
2025-02-27 17:28:39 -0600

    correccion de sintaxis de libs a lib

--- obj/Debug/net8.0/CrudProductos.AssemblyInfo.cs
+++ obj/Debug/net8.0/CrudProductos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrudProductos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a20ead26fe89f04f2c2ed80718b7948713b13fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6098a703b4069267f8dc7d713ea4a8b79b23eac")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrudProductos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrudProductos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
